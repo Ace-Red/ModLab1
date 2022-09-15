acd6a02a3e1de3c3a99b63f972b8840368240706
Nikita Sakun <NikitaSakun1@gmail.com>
2022-09-15 13:08:44 +0300

    Лаба 1

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,65 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Random;
 
 
 public class Main {
-
+    static int n = 10_000;
 
     public static void main(String[] args) {
-        double[] arr = Generator1.generator(10_000, 2);
+        generator(18);
+    }
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-            if(i%5==0){
-                System.out.println();
-            }
+    public static double[] createExpDist(double[] x, double lambda) {
+        double[] fx = new double[n];
+        for (int i = 0; i < n; i++) {
+            fx[i] = 1 - Math.pow(Math.E, -lambda * x[i]);
         }
-        Generator1.gistograma(arr,50);
-        double midValue = Generator1.midValue(arr);
-        double variance = Generator1.variance(arr,midValue);
+        return fx;
+    }
+
+    public static void generator(double lambda) {
+        // генеруємо випадкові числа
+        double[] x = new double[n];
+        Random random = new Random();
+        for (int i = 0; i < x.length; i++) {
+            x[i] = -Math.log(random.nextDouble()) / lambda;
+        }
+        // рахуємо середнє значення
+        double midValue = 0;
+        for (double v : x) {
+            midValue += v;
+        }
+        midValue = (midValue / n);
+        System.out.println("Середнє значення: " + midValue);
+        // рахуємо середнє дисперсію
+        double variance = 0;
+        for (double v : x) {
+            variance += (Math.pow((v - midValue), 2));
+        }
+        variance = variance / n;
+        System.out.println("Дисперсія: " + variance);
+        // рахуємо функцію розподілу
+        double[] fx = createExpDist(x, lambda);
+
+//        try (PrintWriter writer = new PrintWriter(new File("result2.csv"))) {
+//            StringBuilder sb = new StringBuilder();
+//            for (int i = 0; i < n; i++) {
+//
+//                sb.append((fx[i]+"").replace(".",","));
+//                sb.append(";");
+//                sb.append( (x[i]+"").replace(".",","));
+//                sb.append("\n");
+//
+//            }
+//            writer.write(sb.toString());
+//            writer.close();
+//        } catch (FileNotFoundException e) {
+//            System.out.println(e.getMessage());
+//
+//        }
+
+
     }
 }
